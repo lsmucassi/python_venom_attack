d321026c4119e0cb11a044f94d4ab995af06bc52
lsmucassi <lindasmucassi@gmail.com>
2020-09-11 23:03:35 +0200

    views & settings

--- API/django_framework/tutorial/tutorial/tutorial/urls.py
+++ API/django_framework/tutorial/tutorial/tutorial/urls.py
@@ -15,7 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include, path
+from rest_framework import routers
+from tutorial.quickstart import views
+
+router = routers.DefaultRoute()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
 
 urlpatterns = [
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
     path('admin/', admin.site.urls),
 ]
