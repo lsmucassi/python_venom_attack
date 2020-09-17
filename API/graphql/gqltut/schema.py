6a3b480d2edcb28f197fa3de8fc38172b18b026f
lsmucassi <lindasmucassi@gmail.com>
2020-09-17 18:25:24 +0200

    schemaquery

--- API/graphql/gqltut/schema.py
+++ API/graphql/gqltut/schema.py
@@ -1,23 +1,41 @@
 import graphene
 import json
+from datetime import datetime
+
+class User(graphene.ObjectType):
+    id = graphene.ID()
+    username = graphene.String()
+    last_login = graphene.DateTime()
+
 
 class Query(graphene.ObjectType):
-    is_staff = graphene.Boolean()
+    users = graphene.List(User, first=graphene.Int())
+    
+    # is_staff = graphene.Boolean()
+    # or is_staff = graphene.Boolean(name='is_staff) to keep camel case
 
-    def resolve_is_stuff(self, info):
-        return True
+    def resolve_users(self, info, first):
+        return [
+            User(username='Ntombi', last_login=datetime.now()),
+            User(username='Sizwe', last_login=datetime.now()),
+            User(username='Nonhle', last_login=datetime.now())
+        ][:first]
 
 
 schema = graphene.Schema(query=Query)
+# or schema = graphene.Schema(query=Query, auto_camelcase=False)
+# to keep camel case
 
 results = schema.execute(
         '''
         {
-            isStaff
+            users(first: 2) {
+                username
+                lastLogin
+            }
         }
         '''
 )
 
-print(results.data.items())
-
-
+items = dict(results.data.items())
+print(json.dumps(items, indent=4))
