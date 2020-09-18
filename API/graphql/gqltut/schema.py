d0ae6ea77287c109b426e96cc73d14e96d39fe01
lsmucassi <lindasmucassi@gmail.com>
2020-09-18 10:18:27 +0200

    schema mutate

--- API/graphql/gqltut/schema.py
+++ API/graphql/gqltut/schema.py
@@ -5,7 +5,7 @@ from datetime import datetime
 class User(graphene.ObjectType):
     id = graphene.ID()
     username = graphene.String()
-    last_login = graphene.DateTime()
+    last_login = graphene.DateTime(required=False)
 
 
 class Query(graphene.ObjectType):
@@ -21,17 +21,33 @@ class Query(graphene.ObjectType):
             User(username='Nonhle', last_login=datetime.now())
         ][:first]
 
+class CreateUser(graphene.Mutation):
 
-schema = graphene.Schema(query=Query)
+    class Arguments:
+        username = graphene.String()
+    
+    user = graphene.Field(User)
+
+    def mutate(self, info, username):
+        user = User(username=username)
+        return CreateUser(user=user)
+
+
+class Mutation(graphene.ObjectType):
+    create_user = CreateUser.Field()
+
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
 # or schema = graphene.Schema(query=Query, auto_camelcase=False)
 # to keep camel case
 
 results = schema.execute(
         '''
-        {
-            users(first: 2) {
-                username
-                lastLogin
+        mutation createUser {
+            createUser(username: "Bob") {
+                user {
+                    username
+                }
             }
         }
         '''
---
+++ API/graphql/gqltut/test.py
@@ -0,0 +1,82 @@
+# if (9 < 0) and (0 < -9):
+#     print("hello")
+# elif (9 > 0) or False:
+#     print("good")
+# else:
+#     print("bad")
+
+# data = [[[1,2], [3,4]],[[5,6],[7,8]]]
+# print(data[1][0][0])
+
+# data = {0:'a', 1:'b', 2:'c'}
+# for i in data.keys():
+#     print(data[i])
+
+# print("xyyzxyzxzxyy".count("yy", 2))
+
+# print('there are %d %s birds.'%(4, 'blue'))
+
+# z = set('abc')
+# z.add('san')
+# z.update(set(['p','q']))
+# print(z)
+
+# print('xyyzxxyxyy'.lstrip('xyy'))
+
+# s="hello"
+# print(type(s))
+
+# print('the {} side {1} {2}'.format('bright', 'of', 'life'))
+
+# d = {"john": 40, "peter":45}
+# print("john" in d)
+
+# print('abcdefcdghcd'.split('cd', 0))
+
+# print(list(enumerate([2, 3])))
+
+# str = "hello"
+# print(str[-1:])
+
+# print(all([2, 4, 0, 6]))
+
+# for i in [1, 2, 3, 4][::-1]:
+#     print(i)
+
+# print(3*1**3)
+
+# names = ['Elton', 'Edith', 'Carl']
+# if 'elton' in names:
+#     print(1)
+# else:
+#     print(2)
+
+# s = ["silicon", "valley", "sf"][(w.upper(),len(w)) for w in s]
+
+# print([index.lower() for index in "hello"])
+
+# lst = [3, 4, 6, 1, 2]
+# lst[1:2] = [7,8]
+# print(lst)
+
+# s = {4 >3, 0, 3-3}
+# print(all(s))
+# print(any(s))
+
+# a = { 1: "a", 2: "b", 3: "c"}
+# b = a.copy()
+# b[2] = "d"
+# print(a)
+
+# a = {'b':5, 'a':9, 'c':7}
+# print(a.sorted(a))
+
+# a = {4, 5, 6}
+# b = {2, 8, 6}
+# print(a - b)
+
+# a = (2, 3, 4)
+# print(sum(a,3))
+
+# print(bool('False') and bool('False'))
+# print(bool())
