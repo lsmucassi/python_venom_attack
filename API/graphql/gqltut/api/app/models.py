a49e947625c58ce994ec98bd3c7726787cea056e
lsmucassi <lindasmucassi@gmail.com>
2020-09-18 13:45:08 +0200

    adding django int

--- API/graphql/gqltut/api/app/models.py
+++ API/graphql/gqltut/api/app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Snippet(models.Model):
+    title = models.CharField(max_length=100)
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+    def body_preview(self):
+        return self.body[:50]
+        
