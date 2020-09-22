2531cf12e7c026570d72a6a541956d5052b40d2a
lsmucassi <lindasmucassi@gmail.com>
2020-09-22 21:52:11 +0200

    tup half

--- basics/tuples.py
+++ basics/tuples.py
@@ -1,1 +1,41 @@
-# Tuples
+# Tuples : Ordered, Immutable, allows Duplicate elements
+my_tuple = ("23", 76, "Hello") # use parantheses
+my_tuple1 =  "23", 76, "Hello" # still creates a tuple
+my_tuple2 = ("23") # a string
+my_tuple3 = ("23",) # a tuple
+my_tuple4 = "23", # tuple
+print(type(my_tuple))
+print(type(my_tuple1))
+print(type(my_tuple2))
+print(type(my_tuple3))
+print(type(my_tuple4))
+
+print("\n")
+# tuple from a list 
+my_tuple1 = tuple([1,2,3,4,5,6])
+print(my_tuple1)
+
+print("\n")
+# access a tuple with indexes 
+item = my_tuple1[0]
+print(item) # first item
+print(my_tuple1[-1]) # last item
+print(my_tuple1[5]) # last item
+
+# can not add as tuples are immutable
+# item[1] = 4
+# print(item)
+
+print("\n")
+
+# iterate 
+for i in my_tuple1:
+    print(i)
+
+a = ("Hello", 42, "World")
+if "Hello" in a:
+    print("yes")
+else:
+    print("no")
+
+print("\n")
