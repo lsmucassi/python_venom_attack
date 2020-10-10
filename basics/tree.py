04b289d73c1518bf2e2983a723d61bb502c70f70
lsmucassi <lindasmucassi@gmail.com>
2020-10-10 18:36:25 +0200

    trees

--- basics/tree.py
+++ basics/tree.py
@@ -9,6 +9,9 @@ class Times (Expr):
         self.left = left
         self.right = right
 
+    def __str__(self):
+        return str(self.left) + "x" + str(self.right)
+
 class Plus (Expr):
     '''
         addition handler
@@ -17,13 +20,26 @@ class Plus (Expr):
         self.left = left
         self.right = right
 
+    def __str__(self):
+        return str(self.left) + "+" + str(self.right)
+
 class Const (Expr):
     def __init__(self, val):
         self.val = val
 
+    def __str__(self):
+        return str(self.val)
+
 class Var (Expr):
         def __init__(self, name):
             self.name = name
+        
+        def __str__(self):
+            return self.name
 
 
 e1 = Times(Const(3),Plus(Var("y"), Var("x")))
+e2 = Plus(Times(Const(3), Var("y")), Var("x"))
+
+print(e1)
+print(e2)
