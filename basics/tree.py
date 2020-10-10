5d8bb28d077d9548af977c5f62e5cf09494ec6d0
lsmucassi <lindasmucassi@gmail.com>
2020-10-10 18:51:29 +0200

    trees eval

--- basics/tree.py
+++ basics/tree.py
@@ -10,7 +10,10 @@ class Times (Expr):
         self.right = right
 
     def __str__(self):
-        return str(self.left) + "x" + str(self.right)
+        return "(" + str(self.left) + "*" + str(self.right) + ")"
+
+    def eval(self, env):
+        return self.left.eval(env) * self.right.eval(env)
 
 class Plus (Expr):
     '''
@@ -21,7 +24,10 @@ class Plus (Expr):
         self.right = right
 
     def __str__(self):
-        return str(self.left) + "+" + str(self.right)
+        return "(" + str(self.left) + "+" + str(self.right) + ")"
+
+    def eval(self, env):
+        return self.left.eval(env) + self.right.eval(env)
 
 class Const (Expr):
     def __init__(self, val):
@@ -29,6 +35,9 @@ class Const (Expr):
 
     def __str__(self):
         return str(self.val)
+    
+    def eval(self, env):
+            return self.val
 
 class Var (Expr):
         def __init__(self, name):
@@ -37,9 +46,15 @@ class Var (Expr):
         def __str__(self):
             return self.name
 
+        def eval(self, env):
+            return env[self.name]
 
+env = {"x": 2, "y": 4}
 e1 = Times(Const(3),Plus(Var("y"), Var("x")))
 e2 = Plus(Times(Const(3), Var("y")), Var("x"))
 
 print(e1)
 print(e2)
+
+print(e1.eval(env))
+print(e2.eval(env))
