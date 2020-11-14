ea296904f168bb09a74809c54e6f3b368e9bb347
lsmucassi <lindasmucassi@gmail.com>
2020-11-14 16:33:00 +0200

    Question 2: add cities

--- the_code_house/logic/alarm_clock/test_pract.py
+++ the_code_house/logic/alarm_clock/test_pract.py
@@ -3,10 +3,10 @@ from alarm_clock import alarm_clock
 
 class TestAlarmClock(unittest.TestCase):
     def test_clock(self):
-        self.assertEqual(alarm_clock(1, False), '07:00') 
+        self.assertEqual(alarm_clock(1, False), '07:00')
         print("Test 1: alarm_clock(1, False), '07:00'")
-        self.assertEqual(alarm_clock(5, False), '07:00')  
-        self.assertEqual(alarm_clock(0, False), '10:00')  
+        self.assertEqual(alarm_clock(5, False), '07:00')
+        self.assertEqual(alarm_clock(0, True), 'off')
 
 if __name__ == '__main__':
     unittest.main()
