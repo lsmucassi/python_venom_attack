ebee577ba69ccda29657eaf16ba5eb6005127082
lsmucassi <lindasmucassi@gmail.com>
2021-11-12 20:09:35 +0200

    giyant: link sorting

---
+++ basics/guess_number/main.py
@@ -0,0 +1,13 @@
+'''
+Ask a user to guess a number
+generate a rondom number and check if it matches the user's input
+tell user if their guess is correct, too high or too low until they get it right
+enter 0 to exit
+'''
+
+
+
+
+
+'''
+create a program that guesses the user's input number'''
---
+++ basics/rock_paper_scissors/main.py
@@ -0,0 +1,3 @@
+'''
+create a program that allows the user to play rock paper scissors with a computer
+r > a, s > p, p > r'''
