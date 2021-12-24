10e061ca01f318be147cd47ec7155b8253ade86d
lsmucassi <lindasmucassi@gmail.com>
2021-12-24 11:25:52 +0200

    DataScience:init Project App 1- complete

--- data_science/12_DSProjects/simple/stock_price/app.py
+++ data_science/12_DSProjects/simple/stock_price/app.py
@@ -2,15 +2,16 @@ import yfinance as yf
 import streamlit as st
 import pandas as pd
 
-st.write(
-    """
-    # A Simple Stock Price App
-    Shown are the stock closong price for Google!
-    """
-)
 
 # define ticker symbol
 ticker_symbol = 'GOOGL'
+
+st.write(
+    f"""
+    # A Simple Stock Price App
+    Shown are the stock ***closong price*** and ***Volume*** for {ticker_symbol}!
+    """
+)
 
 # get data on the ticker symbol
 ticker_data = yf.Ticker(ticker_symbol)
@@ -18,5 +19,7 @@ ticker_data = yf.Ticker(ticker_symbol)
 # get historical prices for this ticker
 ticker_df = ticker_data.history(period='id', start='2010-5-31', end='2020-5-31')
 
+st.write("""### Closing Price""")
 st.line_chart(ticker_df.Close)
+st.write("""### Volume""")
 st.line_chart(ticker_df.Volume)
