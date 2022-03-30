ed8b9117e02e6217b7ce7554e8188a6c0df3b2b2
lsmucassi <lindasmucassi@gmail.com>
2022-03-30 11:47:50 +0200

    algos module

--- basics/algos/search_algos/binary_search.py
+++ basics/algos/search_algos/binary_search.py
@@ -7,6 +7,16 @@ Binary search compares the target value to the middle element of the array
 '''
 a_list = [54,5,45,74,587,87,8,454,5,8,7,61,76,94,45,3,23,2,68,98,97,37,132,135,64,89,91,3,18]
 
-def binary_search(list, value, left_edge, right_edge):
-     if left_edge <= right_edge:
-         middle
+def binary_search(a_list, value, left_edge, right_edge):
+    if left_edge <= right_edge:
+        middle = left_edge + (right_edge - 1) // 2
+        if a_list[middle] == value:
+             return middle
+        elif a_list[middle] < value:
+            return binary_search(a_list, value, middle + 1, right_edge)
+        else:
+            return binary_search(a_list, value, left_edge, middle - 1)
+    
+    return -1
+
+print(a_list, 8, 0, len(a_list)- 1)
